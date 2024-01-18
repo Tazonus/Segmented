432accd56704abd152520b6aeff514cc928dc538
Taz <kontopgo1@gmail.com>
2024-01-18 12:49:57 +0100

    added: - shortcuts

--- main.py
+++ main.py
@@ -1,20 +1,62 @@
 import tkinter as tk
 from tkinter import ttk
 import Segmenter_file as seg
+from pynput import keyboard
+
+
 
 class SegmenterGUI:
-    def __init__(self,):
+    def __init__(self):
         self.root = tk.Tk()
+        self.segmenter = seg.Segmenter()
+        listener = keyboard.Listener(on_press=self.keyPressed,on_release=self.keyReleased)
+        listener.start()
+
+        self.keyShortcuts = [("'s'","segment")]
+        self.shortcutBuffor = ""
+
+        self.setup()
+        #Main loop:
+        self.update_labels()
+        self.root.mainloop()
+        
+
+### INPUT HANDLING ###
+    def keyReleased(self,key):
+        character = str(key)
+        self.shortcutBuffor = self.shortcutBuffor.replace(character, '')
+        
+    def keyPressed(self,key):
+        self.shortcutBuffor += str(key)
+
+        if 'Key.ctrl' in self.shortcutBuffor and 'Key.alt' in self.shortcutBuffor:
+            for x in self.keyShortcuts:
+                if self.shortcutBuffor.find(x[0]) > 0:
+                    self.handle_button_click(x[1])
+
+#Handles button click
+    def handle_button_click(self, action):
+        if action == "segment":
+            self.segmenter.segment()
+        elif action == "stop":
+            self.segmenter.stop()
+        elif action == "reset":
+            self.segmenter.reset()
+        elif action == "exit":
+            self.root.destroy()  
+
+### GRAPHICAL INTERFACE ###
+    def setup(self):
         self.root.title("SegmentDead")
         self.root.geometry("500x450")
-        self.segmenter = seg.Segmenter()
 
-        #TLabel displaying title
+        #Label displaying title
         self.title_var = tk.StringVar()
         self.title_var.set(self.segmenter.title)
         title_label = ttk.Label(self.root, textvariable=self.title_var, font=('Helvetica', 18))
         title_label.grid(row=0, column=1, columnspan=4, pady= [8,0])
 
+        #Label displaying category
         self.category_var = tk.StringVar()
         self.category_var.set(self.segmenter.category)
         category_label = ttk.Label(self.root, textvariable=self.category_var, font=('Helvetica', 12))
@@ -27,7 +69,6 @@ class SegmenterGUI:
             ("Reset", "reset"),
             ("Exit", "exit")
         ]
-
         for i, (text, action) in enumerate(buttons):
             button = ttk.Button(self.root, text=text, command=lambda a=action: self.handle_button_click(a))
             button.grid(row=i + 2, column=0, pady=5)
@@ -46,30 +87,15 @@ class SegmenterGUI:
         segment_time_label = ttk.Label(self.root, textvariable=self.segmenter.segment_time_label_var, font=('Helvetica', 12))
         segment_time_label.grid(row=14, column=1, columnspan=4, pady=5)
 
-        #Main loop:
-        self.update_labels()
-        self.root.mainloop()
-    
-    #Handles button click
-    def handle_button_click(self, action):
-        if action == "segment":
-            self.segmenter.segment()
-        elif action == "stop":
-            self.segmenter.stop()
-        elif action == "reset":
-            self.segmenter.reset()
-        elif action == "exit":
-            self.root.destroy()
-
-    #Updates list of segmments
-    #by clearing whole list and filling it again
+#Updates list of segmments
+#by clearing whole list and filling it again
     def update_segment_list(self):
         self.segment_list.delete(0, tk.END) 
         for line in self.segmenter.curent_run_lines:
             self.segment_list.insert(tk.END, line)
 
-    #Updates labels
-    #loops via self-invocation, loop end when root.destroy() is invoked
+#Updates labels
+#loops via self-invocation, loop end when root.destroy() is invoked
     def update_labels(self):
         current_time = self.segmenter.get_curent_time()
         segment_time = self.segmenter.get_segment_time()
