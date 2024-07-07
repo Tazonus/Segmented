2cbd32cd33d4ad3250c5627a3e1ee5c010cf33f7
Tazonus <Filip.herbik@gmail.com>
2024-07-07 03:08:59 +0200

    ADD Documentation - extracted functions from setup()

--- Segmenter.py
+++ Segmenter.py
@@ -7,9 +7,6 @@ class Segmenter:
         ## permanent storage:
         self.title = "Starcraft 2 - Wings of Liberty"
         self.category = "Any% Normal"
-        
-        # these will go to tuple:
-        
         self.segments_time = []
         self.segment_names = [
             "Liberation Day", "The Outlaws", "Zero Hour", "Smash and Grab",
@@ -18,7 +15,7 @@ class Segmenter:
             "A Sinister Turn", "Echoes of the Future", "The Moebius Factor",
             "Supernova", "Maw of the Void", "The Gates of Hell", "Shatter the Sky", "All In"
         ]
-        #stacraft 2 as place holder
+        # segment_times and segment_names will be merged into a tuple
 
         ## temporary storage:
         self.is_running = False
@@ -26,14 +23,16 @@ class Segmenter:
         self.last_segment_time = 0
         self.current_segment = 0
         self.curent_run_lines = []
+
         self.reset()
 
 ### TEXT EDIT ### - this section takes hold of making data easy to read
-##   later i will make its own class so it could handle user customisation
-    
-    #inputs     time
-    #returns    string which displays time in form of hh:mm:ss
+##   later i will make its own class so it will be easier to add user customisation
     def format_time(self, seconds):
+        """Formats time into readable form
+        Inputs  time in seconds
+        returns string which displays time in form of hh:mm:ss
+        """
         minutes, seconds = divmod(seconds, 60)
         hours, minutes = divmod(minutes, 60)
         
@@ -44,19 +43,14 @@ class Segmenter:
                 return f"{int(minutes):02}:{seconds:.2f}"
         else:
             return f"{int(hours):02}:{int(minutes):02}:{seconds:.2f}"
-
-    #inputs     time
-    #returns    easy to read text about segment
     def make_line(self, current_time, segment):
+        '''returns strng with info about segment'''
         return (f"{self.segment_names[segment]}: {self.format_time(current_time)}")
-        #in progress
-        
+        #in progress     
 
-### TIME MANAGMENT ### - this section takes hold of the segmenting process
-
-    #starts the segmenting
-    #for caution sake it invokes reset() so every temp data(line 22) will be set to default
+### TIME MANAGMENT ### - takes hold of the segmenting process
     def start(self):
+        '''Starts segmenting after reseting all temporary data'''
         if not self.is_running:
             self.reset()
             self.start_time = time.time()
@@ -64,19 +58,16 @@ class Segmenter:
             while len(self.curent_run_lines) < len(self.segment_names):
                 self.curent_run_lines.append(0)
         print("Start time.")
-
-    #stops the segmenting
-    #ending run quickly
     def stop(self):
+        '''stops segmenting ending run quickly'''
         if self.is_running:
             elapsed_time = time.time() - self.start_time
             self.is_running = False
             print(f"Stopped. Total time: {self.format_time(elapsed_time)}")
         else:
             print("There is nothing to be stoped :P")
-
-    #resets all data
     def reset(self):
+        "Resets all temporary data"
         self.is_running = False
         self.start_time = 0
         self.last_segment_time = 0
@@ -87,9 +78,8 @@ class Segmenter:
             self.curent_run_lines.append(0)
             
         print("Reset time.")
-
-    #segments timestamps
     def segment(self):
+        '''segments timestamps'''
         if self.is_running:
             print(f"Section {self.current_segment+1}.")
 
@@ -111,14 +101,12 @@ class Segmenter:
             self.start()
 
 ### GETTERS ###
-
     def get_curent_time(self):
         if self.is_running:
             curent_time = time.time() - self.start_time
             return self.format_time(curent_time)
         else:
             return self.format_time(self.last_segment_time)
-
     def get_segment_time(self):
         if self.is_running:
             segment_time = time.time() - self.start_time - self.last_segment_time
--- main.py
+++ main.py
@@ -1,43 +1,40 @@
 import tkinter as tk
 from tkinter import ttk
-import Segmenter_file as seg
 from pynput import keyboard
 
-
+import Segmenter as seg
 
 class SegmenterGUI:
     def __init__(self):
-        self.root = tk.Tk()
         self.segmenter = seg.Segmenter()
+        self.root = tk.Tk()
         listener = keyboard.Listener(on_press=self.keyPressed,on_release=self.keyReleased)
-        listener.start()
-
         self.keyShortcuts = [("'s'","segment")]
         self.shortcutBuffor = ""
 
+        self.root.title("SegmentDead")
+        self.root.geometry("500x520")
         self.setup()
+        listener.start()
+
         self.update_segment_list()
-        #Main loop:
         self.update_labels()
         self.root.mainloop()
 
-        
-
-### INPUT HANDLING ###
+### INPUT HANDLING
     def keyReleased(self,key):
+        '''Handles Releasing a key, used with shortcuts'''
         character = str(key)
         self.shortcutBuffor = self.shortcutBuffor.replace(character, '')
-        
     def keyPressed(self,key):
+        '''Handles Pressing a key, used with shortcuts'''
         self.shortcutBuffor += str(key)
-
         if 'Key.ctrl' in self.shortcutBuffor and 'Key.alt' in self.shortcutBuffor:
             for x in self.keyShortcuts:
                 if self.shortcutBuffor.find(x[0]) > 0:
                     self.handle_button_click(x[1])
-
-#Handles button click
     def handle_button_click(self, action):
+        '''Handles button click'''
         if action == "segment":
             self.segmenter.segment()
         elif action == "stop":
@@ -47,24 +44,28 @@ class SegmenterGUI:
         elif action == "exit":
             self.root.destroy()  
 
-### GRAPHICAL INTERFACE ###
+### Setup
     def setup(self):
-        self.root.title("SegmentDead")
-        self.root.geometry("500x450")
-
-        #Label displaying title
+        self.setup_title(self.segmenter.title)
+        self.setup_category(self.segmenter.category)
+        self.setup_buttons()
+        self.setup_curentTime()
+        self.setup_list()
+        
+    def setup_title(self, title):
+        '''Setups label displaying title'''
         self.title_var = tk.StringVar()
-        self.title_var.set(self.segmenter.title)
+        self.title_var.set(title)
         title_label = ttk.Label(self.root, textvariable=self.title_var, font=('Helvetica', 18))
-        title_label.grid(row=0, column=1, columnspan=1000, pady= [8,0])
-
-        #Label displaying category
+        title_label.grid(row=0, column=1, columnspan=1000, pady= [8,0])   
+    def setup_category(self, category):
+        '''Setups label displaying category'''
         self.category_var = tk.StringVar()
-        self.category_var.set(self.segmenter.category)
+        self.category_var.set(category)
         category_label = ttk.Label(self.root, textvariable=self.category_var, font=('Helvetica', 12))
         category_label.grid(row=1, column=1, columnspan=1000)
-
-        #Buttons for different actions
+    def setup_buttons(self):
+        '''Setups button to segment, stop, reset and exit program'''        
         buttons = [
             ("Segment", "segment"),
             ("Stop", "stop"),
@@ -74,12 +75,9 @@ class SegmenterGUI:
         for i, (text, action) in enumerate(buttons):
             button = ttk.Button(self.root, text=text, command=lambda a=action: self.handle_button_click(a))
             button.grid(row=i + 2, column=0, pady=5)
-
-        #List displaying split times
-        self.segment_list = tk.Listbox(self.root, selectmode=tk.BROWSE, font=('Helvetica', 10), width=50, height=20)
-        self.segment_list.grid(row=2, column=1, columnspan=1, padx=3, pady=10, rowspan=10)
-        
-        #Label displaying current time
+    def setup_curentTime(self):
+        '''Setups display for curent time, and curent segment time'''
+                #Label displaying current time
         self.segmenter.current_time_label_var = tk.StringVar()
         current_time_label = ttk.Label(self.root, textvariable=self.segmenter.current_time_label_var, font=('Helvetica', 16))
         current_time_label.grid(row=13, column=1, columnspan=4, pady=5)
@@ -88,20 +86,22 @@ class SegmenterGUI:
         self.segmenter.segment_time_label_var = tk.StringVar()
         segment_time_label = ttk.Label(self.root, textvariable=self.segmenter.segment_time_label_var, font=('Helvetica', 12))
         segment_time_label.grid(row=14, column=1, columnspan=4, pady=5)
+    def setup_list(self):
+        '''Setups list for displaying segments time'''
+        self.segment_list = tk.Listbox(self.root, selectmode=tk.BROWSE, font=('Helvetica', 10), width=50, height=20)
+        self.segment_list.grid(row=2, column=1, columnspan=1, padx=3, pady=10, rowspan=10)
 
-#Updates list of segmments
-#by clearing whole list and filling it again
+### Update
     def update_segment_list(self):
+        '''Updates list of segments by clearin whole list and filling it again'''
         self.segment_list.delete(0, tk.END) 
         i = 0
         for line in self.segmenter.curent_run_lines:
             time_stamp = self.segmenter.make_line(line, i)
             self.segment_list.insert(tk.END, time_stamp)
             i += 1
-
-#Updates labels
-#loops via self-invocation, loop end when root.destroy() is invoked
     def update_labels(self):
+        '''Updates labels'''
         current_time = self.segmenter.get_curent_time()
         segment_time = self.segmenter.get_segment_time()
 
